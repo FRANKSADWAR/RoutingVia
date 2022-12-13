1beaad1106694449119580812e781f36dc21758c
FRANKSADWAR <billyfranks98@gmail.com>
2022-12-13 14:25:53 +0300

    added home page URL

--- testapp/multiroutes/urls.py
+++ testapp/multiroutes/urls.py
@@ -3,14 +3,11 @@ from multiroutes import views
 
 
 urlpatterns =[
-    path(),
-
+    path('',views.home_page,name='home'),
 
     ## NEW URL Functionalities
     path('nearest_vertex/<float:lng>/<float:lat>/',views.get_nearest_vertex,name='nearest_vertex'),
-    
     path('get_shortest_path/<int:source>/<int:target>/',views.get_shortest_path),
-
     path('get_shortest_path_obstacles/<int:source>/<int:target>/',views.get_shortest_path_obstacles),
 
 ]
